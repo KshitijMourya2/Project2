fa944e8adaf5ac0ffebf476ff7effdab3c67c8e3
kshitijmourya <mourya.k@husky.neu.edu>
2018-04-20 04:39:25 -0400

    Responsive Collapsing but not expanding

--- cryptoapp/config/dev.exs
+++ cryptoapp/config/dev.exs
@@ -53,6 +53,6 @@ config :cryptoapp, Cryptoapp.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "cryptoapp",
   password: "cryptoapp",
-  database: "cryptoapp_dev_final",
+  database: "cryptoapp_dev",
   hostname: "localhost",
   pool_size: 10
